2624ae1b38ba9391df15f5010aa140cdf52bb7f4
lada <nicklada94@gmail.com>
2020-03-28 20:03:28 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int price = 10000;
+        int miles = price/cost;
+        return miles;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(20);
+        System.out.println(miles);
+    }
+}
